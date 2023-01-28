27bb385c8175d7b7fd13adcf0afe067f92102316
Joohwan Lee <aperire0402@gmail.com>
2023-01-28 05:07:49 -0500

    add validation accounts

--- src/contracts/OpenBrowser.sol
+++ src/contracts/OpenBrowser.sol
@@ -2,179 +2,102 @@ pragma solidity ^0.8.15;
 import "./IERC20.sol";
 
 contract OpenBrowser {
-    // File Publickey Array
-    string[] public pubkeyArray;
+    /*
+    Client Associated Data
+    */
+    uint public pubkeyArrayLength;
+    string[] public pubkeyArray; // [pubkey]
+    mapping(string => DataInfo) public dataInfoMap; // pubkey => DataInfo
+    mapping(address => uint) public clientBalance; // address => MATIC Credit Balance
 
-    // File pubkey: DataInfo mapping
-    mapping(string => DataInfo) public pubkeyDataMap;
-
-    // Client Fund (ETH) locked as credit
-    mapping(address => uint256) public clientCredit;
-
-    // Insurance Fund (ETH) locked by storage providers
-    mapping(address => StorageInfo) public storageInfoMap;
-    mapping(address => uint256) public storageBalance;
-    
-    // Validator Stake (OPB)
-    mapping(address => uint256) public vaildatorStake;
-    uint256 public totalStake;
-
-    // Storage Registration Balance
-    uint storageRegistrationFee = 0;
-
-    // Current Vote Epoch
-    uint currentVoteEpoch = 0;
-
-    // OPB Token Address
-    IERC20 OPB = IERC20(0x87c6eCcD1074108f71843368DE3ccC86274217dF);
-
-    // Data Struct
     struct DataInfo {
-        address[] associatedStorageAddress;
-        string sha256Data;
-        uint price;
-        uint validated; // 1 for validated
+        uint paidAmount;
+        address storageAddress;
+        address creator;
+        bool isValidated;
     }
 
-    // User Struct
-    struct UserInfo {
-        string[] pubkeyArray;
-    }
+    /*
+    Validator Associated Data
+    */
+    uint public totalStakedOpb;
+    uint public currentEpochOpbVoted;
+    uint public numValidators;
+    address[] public validatorAddressArray;
+    mapping(address => ValidatorInfo) public validatorInfoMap;
 
-    // Validator Struct
     struct ValidatorInfo {
-        uint opbStake;
-        uint commissionRate;
-        uint startEpoch;
-        uint voteNum;
-        uint voteSuccessRate;
+        uint opbStakedAmount;
+        uint maticFeeBalance;
+        uint registeredEpoch;
+        uint totalValidVotes;
+        address c;
         address validatorAddress;
     }
 
+    /*
+    Storage Associated Data
+    */
+    uint public numStorage;
+    address[] public storageAddressArray;
+    mapping(address => StorageInfo) public storageInfoMap;
 
-    // Storage Struct
     struct StorageInfo {
         uint pricePerByte;
         string endpoint;
-        uint storageLimit;
-        uint credibilityScore;
+        uint storageByteLimit;
         address storageAddress;
-        uint status; //0 if accepting, 1 if offline
+        uint status; // 0 if online, 1 if offline
+        uint collateralOpbAmount;
+        uint earnedCredit;
     }
 
     /*
-    Global Functions
+    WhistleBlower Associated Data
     */
-    // simulateTransaction: Ran by client
-    function simulateTransaction(
-        uint _byteSize, string memory _pubkey, string memory _sha256data, address[] memory _storageArray, uint[] memory _storageWeight
-    ) public {
-        // Calculate price
-        uint totalPrice = 0;
-        for (uint i=0; i<_storageArray.length; i++) {
-            address storageAddress = _storageArray[i];
-            StorageInfo memory storageInfo = storageInfoMap[storageAddress];
-            uint storagePricePerByte = storageInfo.pricePerByte;
-            uint weight = _storageWeight[i];
-            uint price = storagePricePerByte * weight;
-            totalPrice += price;
-        }
-        require (clientCredit[msg.sender] >= totalPrice, "insufficient credit");
-        DataInfo memory dataInfo = DataInfo(_storageArray, _sha256data, totalPrice, 0);
-        pubkeyDataMap[_pubkey] = dataInfo;
-    }
+    mapping(address => WhistleBlowerInfo) public whistelBlowerInfoMap;
 
-    // voteInvalidStorage: Ran by validator to validate storage
-    function voteInvalidStorage(
-        
-    ) {
-
-    }
-
-    // voteInvalidPubkey: Ran by validator to validate malicious data posted by client
-    function voteInvalidPubkey(
-
-    ) {
-        
+    string[] public whistleBlowerReportArray;
+    
+    struct WhistleBlowerInfo {
+        address whisteBlowerAddress;
+        uint collateralOpbAmount;
+        uint trueReportNum;
+        uint falseReportNum;
     }
 
     /*
-    Client Functions
+    Block Associated Data
     */
-    function clientDepositCredit() public payable {
-        address _client = msg.sender;
-        clientCredit[_client] += msg.value;
-    }
+    BlockInfo[] public blockInfoArray; //Store recent one block
+    uint currentEpoch;
 
-    function clientWithdrawCredit(uint _amount) public payable {
-        require(_amount >= clientCredit[msg.sender], "insufficient balance");
-        address payable _client = payable(msg.sender);
-        clientCredit[msg.sender] -= _amount;
-
-        (bool success, bytes memory data) = _client.call{value: _amount}("");
-        require(success, "failed to withdraw balance");
+    struct BlockInfo {
+        address client;
+        uint paidAmount;
+        address storageAddress;
+        string pubkey;
     }
 
     /*
-    Validator Functions
+    Party Registration Functions
     */
-    function registerValidator(
-        uint _commissionRate,
-        uint _opbStake
-    ) public payable {
-        require(OPB.allowance(msg.sender, address(this)) == _opbStake, "Insufficient Allowance");
-        require(OPB.transferFrom(msg.sender, address(this), _opbStake), "Transfer succesful"); 
-
-        uint startEpoch = currentVoteEpoch;
-        uint voteNum = 0;
-        uint voteSuccessRate = 0;
-        
+    function registerValidator() public {
+        require(validatorInfoMap[msg.sender].validatorAddress = address(0), "Validator is registered");
         ValidatorInfo memory validatorInfo = ValidatorInfo(
-            _opbStake, _commissionRate, startEpoch, voteNum, voteSuccessRate, msg.sender
+            0, 0, currentEpoch, 0, msg.sender
         );
-
-
     }
 
-    function unregisterValidator() {
-
-    }
-
-    function validatorWithdrawFund() {
-
-    }
-
-    function delegateStake() {
-
-    }
-
-    /*
-    Storage Functions
-    */
     function registerStorage(
-        uint _pricePerByte, string memory _endpoint, uint _storageLimit, uint _status
-    ) public payable {
-        // Pay one time register fee (0.1 ETH)
-        require(msg.value==100000000000000000);
-        storageRegistrationFee += msg.value;
-
-        // Register
-        uint defaultCredibilityScore = 50; // 0~100
+        uint _pricePerByte, string memory _endpoint, uint _storageByteLimit
+    ) public {
+        require(storageInfoMap[msg.sender].storageAddress == address(0), "Storage is registered");
         StorageInfo memory storageInfo = StorageInfo(
-            _pricePerByte, _endpoint, _storageLimit, defaultCredibilityScore, msg.sender, _status
+            _pricePerByte, _endpoint, _storageByteLimit, msg.sender, 1, 0, 0
         );
-        storageInfoMap[msg.sender] = storageInfo;
     }
 
-    function updateStorageInfo() {
-        
-    }
 
-    function updateStorageStatus() {
 
-    }
-
-    function storageWithdrawFund() {
-
-    }
 }
